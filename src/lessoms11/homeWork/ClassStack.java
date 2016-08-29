d473565e013017a32f530f33c45754821e2c8041
Denni_Asus <Online08.08@mail.ru>
2016-08-29 22:22:25 +0300

    Stack

---
+++ src/lessoms11/homeWork/ClassStack.java
@@ -0,0 +1,62 @@
+package lessoms11.homeWork;
+
+/**
+ * Created by Denni on 29.08.2016.
+ */
+public class ClassStack implements Stack {
+
+    private int maxSize;
+    private int[] array;
+    private int size;
+    private int hesd;
+    private int tail;
+
+    public ClassStack(int maxSize) {
+        this.maxSize = maxSize;
+        array = new int[maxSize];
+        size = 0;
+        hesd = 0;
+        tail = 0;
+    }
+
+
+    @Override
+    public boolean push(int element) {
+        if (hesd <= maxSize) {
+            array[hesd++] = element;
+            size++;
+            return true;
+        }
+        return false;
+    }
+
+
+    @Override
+    public Integer pop() {
+        int res = take();
+        tail++;
+        size--;
+        return res;
+    }
+
+    @Override
+    public Integer take() {
+        if (!isEmpty()) {
+            return array[tail];
+        }
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        if (size == 0) {
+            return true;
+        }
+        return false;
+    }
+}
